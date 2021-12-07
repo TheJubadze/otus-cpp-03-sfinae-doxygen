e493141be9a1591b68e03e3ed40acde97b76bc18
Vitaly <6545415@gmail.com>
2021-12-07 22:54:09 +0300

    update SFINAE

--- src/IpAddress.h
+++ src/IpAddress.h
@@ -3,33 +3,30 @@
 #include <iostream>
 #include <vector>
 
-template<typename T,
-        typename = typename std::enable_if<std::is_integral<T>::value>::type>
-void PrintIp(T ipAddress) {
-    std::cout << ipAddress << "\tshort\n";
+template<typename T>
+typename std::enable_if<std::is_integral<T>::value>::type
+PrintIp(T ipAddress) {
+    std::cout << ipAddress << "\tintegral\n";
 }
 
-template<typename T,
-        typename = typename std::enable_if<
-                std::is_same<
-                        typename std::remove_reference<T>::type,
-                        std::vector<int>>::value,
-                void>::type,
-        typename STUB1 = int>
-void PrintIp(T ipAddress) {
+template<typename T>
+typename std::enable_if<
+        std::is_same<
+                typename std::remove_reference<T>::type,
+                std::vector<int>>::value,
+        void>::type
+PrintIp(T ipAddress) {
     for (auto i: ipAddress)
         std::cout << i << ".";
 
-    std::cout << "\tvector*\n";;
+    std::cout << "\tvector\n";;
 }
 
-template<typename T,
-        typename = typename std::enable_if<
-                std::is_same<T, const char *>::value,
-                void>::type,
-        typename STUB1 = int,
-        typename STUB2 = int>
-void PrintIp(T ipAddress) {
-    std::cout << ipAddress << "\tvector*\n";;
+template<typename T>
+typename std::enable_if<
+        std::is_same<T, const char *>::value,
+        void>::type
+PrintIp(T ipAddress) {
+    std::cout << ipAddress << "\tconst char*\n";;
 }
 
