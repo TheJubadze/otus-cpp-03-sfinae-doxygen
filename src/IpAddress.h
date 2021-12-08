f4890897a5c8a3feac64efe8fc85ef97bbdc9853
Vitaly Senokosov <vitalyse@playtika.com>
2021-12-08 20:27:32 +0300

    update integrals

--- src/IpAddress.h
+++ src/IpAddress.h
@@ -6,15 +6,19 @@
 template<typename T>
 typename std::enable_if<std::is_integral<T>::value>::type
 PrintIp(T ipAddress) {
-    std::cout << ipAddress << "\tintegral\n";
+    auto bytes = reinterpret_cast<unsigned char *>(&ipAddress);
+    for (int i = 0; i < sizeof(T); ++i) {
+        std::cout << static_cast<short>(bytes[i]) << ".";
+    }
+    std::cout << "\tintegral: " << sizeof(T) << " bytes\n";
 }
 
 template<typename T>
 typename std::enable_if<
-        std::is_same<
-                typename std::remove_reference<T>::type,
-                std::vector<int>>::value,
-        void>::type
+    std::is_same<
+        typename std::remove_reference<T>::type,
+        std::vector<int>>::value,
+    void>::type
 PrintIp(T ipAddress) {
     for (auto i: ipAddress)
         std::cout << i << ".";
@@ -24,8 +28,8 @@ PrintIp(T ipAddress) {
 
 template<typename T>
 typename std::enable_if<
-        std::is_same<T, const char *>::value,
-        void>::type
+    std::is_same<T, const char *>::value,
+    void>::type
 PrintIp(T ipAddress) {
     std::cout << ipAddress << "\tconst char*\n";;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,13 @@
 
 int main(int, char **) {
 
+    PrintIp(static_cast<char>(-1));
+    PrintIp(static_cast<short>(0));
+    PrintIp(static_cast<int>(2130706433));
+    PrintIp(static_cast<long>(8875824491850138409));
+
     PrintIp("192.168.0.1");
-    PrintIp((short) 192);
-    PrintIp((int) 19216801);
-    PrintIp((long) 19216802);
     PrintIp(std::vector<int>({192, 168, 0, 1}));
 
     return 0;
 }
-
